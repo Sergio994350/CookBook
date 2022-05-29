ec7fae2607cb14db293eb62ecc9af38b9f627dbb
Sergey Tarasov <tarasov-mnm@yandex.ru>
2022-05-29 10:31:13 +0300

    initial commit

---
+++ app/src/main/java/com/sergio994350/cookbook/DetailActivity.kt
@@ -0,0 +1,11 @@
+package com.sergio994350.cookbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
